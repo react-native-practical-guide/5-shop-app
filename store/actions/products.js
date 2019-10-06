7692f80da0ecc7ccc38d8c92a59b670859c3b744
Fotis Tsakiris <footios76@gmail.com>
2019-10-06 14:12:18 +0300

    Storing products on the server

--- store/actions/products.js
+++ store/actions/products.js
@@ -10,14 +10,42 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-	return {
-		type: CREATE_PRODUCT,
-		productData: {
-			title,
-			description,
-			imageUrl,
-			price
-		}
+	// Note: Now also createProduct returns a Promise
+
+	// async code before dispatching the action...
+	return async (dispatch) => {
+		// With redux thunk `createProduct` action returns a function. This func receives an argument
+		// the `dispatch` func, which will be passed automatically by redux thunk.
+		// Redux thunk will call this `dispatch` func.
+		// But before that you can execute any `async` code you want...
+		const response = await fetch('https://shop-app-bf402.firebaseio.com/products.json', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				// Set in alphabetical order, because this is what Firebase uses...
+				description,
+				imageUrl,
+				price,
+				title,
+			})
+		});
+
+		const resData = await response.json();
+		console.log(resData);
+
+		// Redux thunk will call this `dispatch` func.
+		dispatch({
+			type: CREATE_PRODUCT,
+			productData: {
+				id: resData.name, // `name` is the key that Firebase gives to its ids... 
+				title,
+				description,
+				imageUrl,
+				price
+			}
+		});
 	};
 };
 
@@ -28,7 +56,7 @@ export const updateProduct = (id, title, description, imageUrl) => {
 		productData: {
 			title,
 			description,
-			imageUrl,
+			imageUrl
 		}
 	};
 };
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -18,7 +18,7 @@ export default (state = initialState, action) => {
 			};
 		case CREATE_PRODUCT:
 			const newProduct = new Product(
-				new Date().toString,
+				action.productData.id,
 				'u1',
 				action.productData.title,
 				action.productData.imageUrl,
