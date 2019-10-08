5cddbce351aef147086db774bd131c8cdd9d9342
Fotis Tsakiris <footios76@gmail.com>
2019-10-08 11:29:08 +0300

    storing orders to the server

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,13 +1,32 @@
-// DO NOT FORGET TO EXPORT THE CONSTANTS
 export const ADD_ORDER = 'ADD_ORDER';
 
-// REMEMBER actions reach all reducers. So you can handle this action also in the cart reducer.
-export const addOrder = (cartItems, totalAmount) => {    
-    return {
-        type: ADD_ORDER,
-        orderData: {
-            items: cartItems,
-            amount: totalAmount
+export const addOrder = (cartItems, totalAmount) => {
+	return async (dispatch) => {
+		const date = new Date();
+		const response = await fetch('https://shop-app-bf402.firebaseio.com/orders/u1.json', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				cartItems,
+				totalAmount,
+				date: date.toISOString()
+			})
+		});
+
+        if(!response.ok) {
+            throw new Error('Something went wrong with adding the order on the server!')
         }
-    }
-}
+		const resData = await response.json();
+		dispatch({
+			type: ADD_ORDER,
+			orderData: {
+				id: resData.name,
+				items: cartItems,
+                amount: totalAmount,
+                date: date
+			}
+		});
+	};
+};
--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -2,22 +2,23 @@ import { ADD_ORDER } from '../actions/orders';
 import Order from '../../models/order';
 
 const initialState = {
-	orders: []
+  orders: []
 };
+
 export default (state = initialState, action) => {
-	switch (action.type) {
-		case ADD_ORDER:
-			const newOrder = new Order(
-				new Date().toString(),
-				action.orderData.items,
-				action.orderData.amount,
-				new Date()
-			);
-			return {
-				// ...state,
-				orders: state.orders.concat(newOrder)
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case ADD_ORDER:
+      const newOrder = new Order(
+        action.orderData.id,
+        action.orderData.items,
+        action.orderData.amount,
+        action.orderData.date
+      );
+      return {
+        ...state,
+        orders: state.orders.concat(newOrder)
+      };
+  }
+
+  return state;
 };
